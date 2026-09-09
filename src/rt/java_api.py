f84821b28f0eeb90b501be97ace3521b2858bc63
davidkovach-fuentes <davidkovach-fuentes2027@u.northwestern.edu>
2026-09-09 09:01:05 -0400

    add back file comments

--- src/rt/java_api.py
+++ src/rt/java_api.py
@@ -1,9 +1,10 @@
 import os
 
 import jpype
-import jpype.imports 
+import jpype.imports # This module is imported for its side effects
 
 def ensure_jvm():
+    # JVM initialization logic belongs here
     if not jpype.isJVMStarted():
         jpype.startJVM(
             "--enable-native-access=ALL-UNNAMED",  # Needed to suppress deprecation warning
@@ -12,6 +13,7 @@ def ensure_jvm():
         
 ensure_jvm()
 
+# Import all java constructs here and access them in the rest of the project through this module
 # isort: off
 from dk.brics.automaton import (  # pyright: ignore[reportMissingModuleSource]
     Automaton,
