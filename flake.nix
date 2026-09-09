4d61e07864d68cd9c2c9d90e2ca4c26de67724f2
davidkovach-fuentes <davidkovach-fuentes2027@u.northwestern.edu>
2026-09-08 21:16:27 -0400

    rebuild following impure template

--- flake.nix
+++ flake.nix
@@ -25,68 +25,69 @@
       jdk = pkgs.jdk21;
     in {inherit pkgs python jdk;});
 
+    otherPythonPackages = {
+      pkgs,
+      python,
+    }: let
+      inherit (python.pkgs) buildPythonPackage fetchPypi;
+    in rec {
+      libdash = buildPythonPackage rec {
+        pname = "libdash";
+        version = "0.4.1";
+        pyproject = true;
+        src = fetchPypi {
+          inherit pname version;
+          hash = "sha256-c1g4RJn3zpyf2OB5BOZVlFEHteXuv/SQZR361iSRwXU=";
+        };
+        build-system = [python.pkgs.setuptools];
+        nativeBuildInputs = with pkgs; [autoconf automake libtool];
+        # clang
+        env.CFLAGS = "-std=gnu17";
+        postPatch = lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+          substituteInPlace setup.py --replace-fail 'libtoolize = "glibtoolize"' 'libtoolize = "libtoolize"'
+        '';
+      };
+
+    pash-annotations = buildPythonPackage rec {
+        pname = "pash-annotations";
+        version = "0.2.4";
+        format = "wheel";
+        src = fetchPypi {
+          pname = "pash_annotations";
+          inherit version;
+          format = "wheel";
+          python = "py3";
+          abi = "none";
+          platform = "any";
+          hash = "sha256-/0dab4EzOTM3t+HFpjloGeCDLB4mAcM0T5BcwsbUh9I=";
+        };
+      };
+
+      shasta = buildPythonPackage rec {
+        pname = "shasta";
+        version = "0.5";
+        pyproject = true;
+        src = pkgs.fetchFromGitHub {
+          owner = "binpash";
+          repo = "shasta";
+          rev = "3ec173d6dc96e9007f5b582634f9315eafcac867";
+          hash = "sha256-royjA/t/KZLg8ouFWKyFCiuBKlMDlmBJ9nuXvGqZDbc=";
+        };
+        build-system = [python.pkgs.setuptools];
+      };
+    };
+
     rtPackages = forAllSystems (
       system: let
         inherit (perSystem.${system}) pkgs python jdk;
-        inherit (python.pkgs) buildPythonPackage buildPythonApplication fetchPypi;
-
-        libdash = buildPythonPackage rec {
-          pname = "libdash";
-          version = "0.4.1";
-          pyproject = true;
-          src = fetchPypi {
-            inherit pname version;
-            hash = "sha256-c1g4RJn3zpyf2OB5BOZVlFEHteXuv/SQZR361iSRwXU=";
-          };
-          build-system = [python.pkgs.setuptools];
-          nativeBuildInputs = with pkgs; [autoconf automake libtool];
-          # clang
-          env.CFLAGS = "-std=gnu17";
-          postPatch = lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
-            substituteInPlace setup.py --replace-fail 'libtoolize = "glibtoolize"' 'libtoolize = "libtoolize"'
-          '';
-        };
-
-        pash-annotations = buildPythonPackage rec {
-          pname = "pash-annotations";
-          version = "0.2.4";
-          format = "wheel";
-          src = fetchPypi {
-            pname = "pash_annotations";
-            inherit version;
-            format = "wheel";
-            python = "py3";
-            abi = "none";
-            platform = "any";
-            hash = "sha256-/0dab4EzOTM3t+HFpjloGeCDLB4mAcM0T5BcwsbUh9I=";
-          };
-        };
-
-        shasta = buildPythonPackage rec {
-          pname = "shasta";
-          version = "0.5";
-          pyproject = true;
-          src = pkgs.fetchFromGitHub {
-            owner = "binpash";
-            repo = "shasta";
-            rev = "3ec173d6dc96e9007f5b582634f9315eafcac867";
-            hash = "sha256-royjA/t/KZLg8ouFWKyFCiuBKlMDlmBJ9nuXvGqZDbc=";
-          };
-          build-system = [python.pkgs.setuptools];
-        };
+        inherit (python.pkgs) buildPythonApplication;
+        inherit (otherPythonPackages {inherit pkgs python;}) libdash pash-annotations shasta;
       in
         buildPythonApplication {
           pname = "rt";
           version = "0.1.0";
           pyproject = true;
-          src = lib.fileset.toSource {
-            root = ./.;
-            fileset = lib.fileset.unions [
-              ./src
-              ./pyproject.toml
-              ./README.md
-            ];
-          };
+          src = lib.cleanSource ./.;
           build-system = [python.pkgs.uv-build];
           dependencies = [
             python.pkgs.jpype1
@@ -97,15 +98,6 @@
             shasta
           ];
           pythonRelaxDeps = ["jpype1"];
-          env = {
-            JAVA_HOME = "${jdk}";
-            RT_AUTOMATON_JAR = "${./jars/automaton.jar}";
-          };
-          pythonImportsCheck = [
-            "rt.java_api"
-            "rt.main"
-            "rti.main"
-          ];
           makeWrapperArgs = [
             "--set" "JAVA_HOME" "${jdk}"
             "--prefix" "PATH" ":" "${lib.makeBinPath [jdk]}"
@@ -157,11 +149,13 @@
           pkgs.uv
           jdk
         ];
-
         JAVA_HOME = "${jdk}";
-
+        env = {
+            UV_NO_SYNC = "1";
+            UV_PYTHON_DOWNLOADS = "never";
+        };
         shellHook = ''
-          echo "rt dev shell: uv sync && uv run rt --help"
+          unset PYTHONPATH
         '';
       };
     });
